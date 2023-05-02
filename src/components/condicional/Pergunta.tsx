d503f9696ca22924c08058425db7a2785f1c81fa
Victor Mello <victorsmello93@gmail.com>
2023-05-02 08:01:49 -0300

    Renderização condicional (parte 1)

---
+++ src/components/condicional/Pergunta.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+export default function Pergunta() {
+  const [aberta, setAberta] = useState(false)
+  return (
+    <div className="border border-white rounded-md">
+      <div className="bg-zinc-700 p-5 cursor-pointer">
+        Pergunta
+      </div>
+      <div className="p-5">
+        Resposta
+      </div>
+    </div>
+  )
+}
