5f4b7a0073ac09fc89bfd77b1e6a3ab28a085d09
VictorMello1993 <victorsmello93@gmail.com>
2023-05-06 17:54:09 -0300

    Renderização de lista (produtos)

---
+++ src/constants/produtos.ts
@@ -0,0 +1,32 @@
+const produtos = [
+  {
+    id: 1,
+    nome: "Camisa",
+    preco: 39.9,
+    descricao: "Camisa branca de algodão",
+    imagem: "https://source.unsplash.com/featured/300x300?tshirt"
+  },
+  {
+    id: 2,
+    nome: "Calça Jeans",
+    preco: 99.9,
+    descricao: "Calça azul de algodão",
+    imagem: "https://source.unsplash.com/featured/300x300?jeans"
+  },
+  {
+    id: 3,
+    nome: "Tênis",
+    preco: 499.99,
+    descricao: "Tênis esportivo",
+    imagem: "https://source.unsplash.com/featured/300x300?sneakers"
+  },
+  {
+    id: 4,
+    nome: "Camisa social",
+    preco: 79.9,
+    descricao: "Camisa social branca",
+    imagem: "https://source.unsplash.com/featured/300x300?shirt"
+  },
+]
+
+export default produtos
