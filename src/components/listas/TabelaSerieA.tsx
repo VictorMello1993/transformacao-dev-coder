bfa1776f2c733e12de8e1004120267eeeb4586a2
VictorMello1993 <victorsmello93@gmail.com>
2023-05-06 17:43:07 -0300

    Renderização de listas

--- src/components/listas/TabelaSerieA.tsx
+++ src/components/listas/TabelaSerieA.tsx
@@ -1,7 +1,16 @@
-export default function TabelaSerieA() {
+interface TabelaSerieAProps {
+  times: string[]
+}
+
+export default function TabelaSerieA(props: TabelaSerieAProps) {
   return (
     <ol>
-      <li>Time</li>
+      {props.times.map((time, index) => {
+        return (
+          <li key={index}
+              className={`text-xl list-decimal ${index % 2 === 0 ? 'text-blue-500' : 'text-yellow-400'}`}>{time}</li>
+        )
+      })}
     </ol>
   )
 }
--- src/pages/listas/basica.tsx
+++ src/pages/listas/basica.tsx
@@ -2,7 +2,7 @@ import { IconBallFootball } from "@tabler/icons-react"
 import TabelaSerieA from "../../components/listas/TabelaSerieA"
 
 export default function PaginaListaBasica() {
-  const tabelaSerieA = [
+  const times = [
     'Botafogo',
     'Fortaleza',
     'Palmeiras',
@@ -17,9 +17,11 @@ export default function PaginaListaBasica() {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="flex items-center text-5xl font-black">Tabela Série A</h1>
-      <IconBallFootball />
-      <TabelaSerieA />
+      <h1 className="flex items-center text-5xl font-black">
+        <IconBallFootball size={50} stroke={1} className="text-green-400"  />
+        Tabela Série A
+      </h1>
+      <TabelaSerieA times={times} />
     </div>
   )
 }
